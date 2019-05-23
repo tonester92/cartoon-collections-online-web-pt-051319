92b8e4db7ec82e1c5ab2c4878237ed6af5a28f2c
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-23 22:01:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,14 +11,13 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls(calls)
-  calls.any? do |call|
-    call.length > 4
+def long_planeteer_calls(battle_cry)
+  battle_cry.any? do |call|
+    battle_cry.length > 4
   end
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length
