f03ed953c446abfaf63abc32a3e0a8c1edb70ba1
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-23 22:03:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
   while i < cheese_types.length
     return cheese_types[i] if array.include?(cheese_types[i])
     i += 1
